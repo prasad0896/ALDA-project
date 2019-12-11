ffcd90726fe2a43f31a9922ae9b1be680dec1c58
Prasad <pushende@ncsu.edu>
2019-12-11 12:16:04 -0500

    Final commit

--- SVM.R
+++ SVM.R
@@ -12,11 +12,11 @@ data <- read.csv("completeFinalDatasetFrom79.csv")
 # summary(data)
 data.df <- data[4:26]
 data.df$WL <- factor(data.df$WL, levels = c("L","W"), labels = c("-1","1"))
-# head(data.df)
-# obj <- tune(svm, WL~., data = data.df, 
-#             ranges = list(gamma = 2^(-1:1), cost = 2^(2:4)),
-#             tunecontrol = tune.control(sampling = "bootstrap")
-# )
+head(data.df)
+obj <- tune(svm, WL~., data = data.df,
+            ranges = list(gamma = 2^(-1:1), cost = 2^(2:4)),
+            tunecontrol = tune.control(sampling = "bootstrap")
+)
 
 print("TSCV Accuracy for SVM - Linear Kernel")
 cost <- -15
@@ -30,10 +30,10 @@ while(cost <= 10) {
   ctr <- 0
   actuals <- NULL
   preds <- NULL
-  while(ctr < 300) {
-    train.df <- data.df[1:(75+ctr),]
+  while(ctr < 210) {
+    train.df <- data.df[1:(52+ctr),]
     # print(1:(75+ctr))
-    test.df <- data.df[(75+ctr+1):(75+ctr+75),]
+    test.df <- data.df[(52+ctr+1):(52+ctr+52),]
     # print((75+ctr+1):(75+ctr+75))
     classifier <- svm(formula = WL ~ .,
                       data = train.df,
@@ -42,10 +42,11 @@ while(cost <= 10) {
                       cost = 2^cost)
     y_pred = predict(classifier, newdata = test.df[-ncol(test.df)])
     preds <- list.append(preds, y_pred)
+
     actuals <- list.append(actuals, test.df[,ncol(test.df)])
-    ctr <- ctr + 75
+    ctr <- ctr + 52
   }
-  actuals <- actuals[-length(actuals)]
+  # actuals <- actuals[-length(actuals)]
   cm = table(actuals, preds)
   res.df[i,1] <- 2^cost
   res.df[i,2] <- mean(actuals == preds)
@@ -69,8 +70,8 @@ write.csv(new.res, file="SVMHyperParLinear.csv", row.names = FALSE)
 #plot(x = log2(res.df$Cost), y = res.df$Accuracy, type = "l", xlab = "Cost (power of 2)", ylab = "TSCV Accuracy", main = "Cost for Linear Kernel", col = "brown")
 plot(x = log2(res.df$Cost), y = scale(res.df$AUC), type = "l", xlab = "Cost (power of 2)", ylab = "Scaled TSCV AUC and Accuracy", main = "Cost for Linear Kernel vs AUC and Accuracy", col = "brown")
 lines(x = log2(res.df$Cost), y = scale(res.df$Accuracy), col = "green")
-points(x = log2(0.25), y = scale(res.df$AUC)[14], col = "blue")
-points(x = log2(0.25), y = scale(res.df$Accuracy)[14], col = "blue")
+points(x = log2(0.125), y = scale(new.res$AUC)[19], col = "blue")
+points(x = log2(0.125), y = scale(new.res$Accuracy)[19], col = "blue")
 legend(-15, 1.2, legend=c("AUC", "Accuracy"),
        col=c("brown", "green"), lty=1:2, cex=0.8)
 
@@ -93,10 +94,10 @@ while(gamma <= 3) {
     ctr <- 0
     actuals <- NULL
     preds <- NULL
-    while(ctr < 300) {
-      train.df <- data.df[1:(75+ctr),]
+    while(ctr < 210) {
+      train.df <- data.df[1:(52+ctr),]
       # print(1:(75+ctr))
-      test.df <- data.df[(75+ctr+1):(75+ctr+75),]
+      test.df <- data.df[(52+ctr+1):(52+ctr+52),]
       # print((75+ctr+1):(75+ctr+75))
       classifier <- svm(formula = WL ~ .,
                         data = train.df,
@@ -107,9 +108,9 @@ while(gamma <= 3) {
       y_pred = predict(classifier, newdata = test.df[-ncol(test.df)])
       preds <- list.append(preds, y_pred)
       actuals <- list.append(actuals, test.df[,ncol(test.df)])
-      ctr <- ctr + 75
+      ctr <- ctr + 52
     }
-    actuals <- actuals[-length(actuals)]
+    # actuals <- actuals[-length(actuals)]
     cm = table(actuals, preds)
     res.df[i,1] <- 2^cost
     res.df[i,2] <- 2^gamma
@@ -142,8 +143,21 @@ plot_ly(res.df, x = ~Cost, y = ~Gamma, z = ~Accuracy, marker = list(color = ~AUC
   layout(scene = list(xaxis = list(title = 'Cost'),
                       yaxis = list(title = 'Gamma'),
                       zaxis = list(title = 'Accuracy')))
+new.res$factor <- 1
+new.res$factor[455] <- 2
+plot_ly(new.res, x = ~Cost, y = ~Gamma, z = ~Accuracy, marker = list(color = ~factor, colorscale = c('#FFE1A1', '#683531'), showscale = TRUE)) %>%
+  add_markers() %>%
+  layout(scene = list(xaxis = list(title = 'Cost'),
+                      yaxis = list(title = 'Gamma'),
+                      zaxis = list(title = 'Accuracy')))
+plot_ly(new.res, x = ~Cost, y = ~Gamma, z = ~AUC, marker = list(color = ~factor, colorscale = c('#FFE1A1', '#683531'), showscale = TRUE)) %>%
+  add_markers() %>%
+  layout(scene = list(xaxis = list(title = 'Cost'),
+                      yaxis = list(title = 'Gamma'),
+                      zaxis = list(title = 'AUC')))
 
-
+# 
+# 
 print("TSCV Accuracy for SVM - Sigmoid Kernel")
 
 gamma <- -15
@@ -163,10 +177,10 @@ while(gamma <= 3) {
     ctr <- 0
     actuals <- NULL
     preds <- NULL
-    while(ctr < 300) {
-      train.df <- data.df[1:(75+ctr),]
+    while(ctr < 210) {
+      train.df <- data.df[1:(52+ctr),]
       # print(1:(75+ctr))
-      test.df <- data.df[(75+ctr+1):(75+ctr+75),]
+      test.df <- data.df[(52+ctr+1):(52+ctr+52),]
       # print((75+ctr+1):(75+ctr+75))
       classifier <- svm(formula = WL ~ .,
                         data = train.df,
@@ -177,9 +191,9 @@ while(gamma <= 3) {
       y_pred = predict(classifier, newdata = test.df[-ncol(test.df)])
       preds <- list.append(preds, y_pred)
       actuals <- list.append(actuals, test.df[,ncol(test.df)])
-      ctr <- ctr + 75
+      ctr <- ctr + 52
     }
-    actuals <- actuals[-length(actuals)]
+    # actuals <- actuals[-length(actuals)]
     cm = table(actuals, preds)
     res.df[i,1] <- 2^cost
     res.df[i,2] <- 2^gamma
@@ -212,21 +226,114 @@ plot_ly(res.df, x = ~Cost, y = ~Gamma, z = ~Accuracy, marker = list(color = ~AUC
   layout(scene = list(xaxis = list(title = 'Cost'),
                       yaxis = list(title = 'Gamma'),
                       zaxis = list(title = 'Accuracy')))
+new.res$factor <- 1
+new.res$factor[430] <- 2
+plot_ly(new.res, x = ~Cost, y = ~Gamma, z = ~Accuracy, marker = list(color = ~factor, colorscale = c('#FFE1A1', '#683531'), showscale = TRUE)) %>%
+  add_markers() %>%
+  layout(scene = list(xaxis = list(title = 'Cost'),
+                      yaxis = list(title = 'Gamma'),
+                      zaxis = list(title = 'Accuracy')))
+plot_ly(new.res, x = ~Cost, y = ~Gamma, z = ~AUC, marker = list(color = ~factor, colorscale = c('#FFE1A1', '#683531'), showscale = TRUE)) %>%
+  add_markers() %>%
+  layout(scene = list(xaxis = list(title = 'Cost'),
+                      yaxis = list(title = 'Gamma'),
+                      zaxis = list(title = 'AUC')))
+
+print("TSCV Accuracy for SVM - Polynomial Kernel")
+
+
+gammas <- NULL
+costs <- NULL
+accuracies <- NULL
+i <- 1
+res.df <- data.frame(Cost=numeric(),
+                     Gamma=numeric(),
+                     Degree = numeric(),
+                     Accuracy=numeric(),
+                     AUC=numeric(),
+                     stringsAsFactors=FALSE)
+
+degree <- 1
+while(degree <= 5) {
+  gamma <- -15
+  while(gamma <= 3) {
+    cost <- -15
+    while(cost <= 6) {
+      ctr <- 0
+      actuals <- NULL
+      preds <- NULL
+      while(ctr < 210) {
+        train.df <- data.df[1:(52+ctr),]
+        # print(1:(75+ctr))
+        test.df <- data.df[(52+ctr+1):(52+ctr+52),]
+        # print((75+ctr+1):(75+ctr+75))
+        classifier <- svm(formula = WL ~ .,
+                          data = train.df,
+                          type = 'C-classification',
+                          kernel = 'polynomial',
+                          cost = 2^cost,
+                          gamma = 2^gamma,
+                          degree = degree)
+        y_pred = predict(classifier, newdata = test.df[-ncol(test.df)])
+        preds <- list.append(preds, y_pred)
+        actuals <- list.append(actuals, test.df[,ncol(test.df)])
+        ctr <- ctr + 52
+      }
+      # actuals <- actuals[-length(actuals)]
+      cm = table(actuals, preds)
+      res.df[i,1] <- 2^cost
+      res.df[i,2] <- 2^gamma
+      res.df[i,3] <- degree
+      res.df[i,4] <- mean(actuals == preds)
+      roc_obj <- roc(actuals, preds)
+      res.df[i,5] <- auc(roc_obj)
+      cost <- cost + 1
+      i <- i + 1
+    }
+    gamma <- gamma + 1
+  }
+  degree <- degree + 1
+}
+
+
+print(res.df[res.df[3] == max(res.df[,3])])
+print(res.df[res.df[4] == max(res.df[,4])])
+attach(res.df)
+new.res <- res.df[order(AUC),]
+detach(res.df)
+write.csv(new.res, file="SVMHyperParPoly.csv", row.names = FALSE)
+# plot_ly(res.df, x = ~Cost, y = ~Gamma, z = ~Accuracy, marker = list(color = ~Accuracy, colorscale = c('#FFE1A1', '#683531'), showscale = TRUE)) %>%
+#   add_markers() %>%
+#   layout(scene = list(xaxis = list(title = 'Cost'),
+#                       yaxis = list(title = 'Gamma'),
+#                       zaxis = list(title = 'Accuracy')))
+plot_ly(res.df, x = ~Cost, y = ~Gamma, z = ~AUC, marker = list(color = ~factor(Degree), colorscale = c('#FFE1A1', '#683531'), showscale = TRUE)) %>%
+  add_markers() %>%
+  layout(scene = list(xaxis = list(title = 'Cost'),
+                      yaxis = list(title = 'Gamma'),
+                      zaxis = list(title = 'AUC')))
+# plot_ly(res.df, x = ~Cost, y = ~Gamma, z = ~Accuracy, marker = list(color = ~AUC, colorscale = c('#FFE1A1', '#683531'), showscale = TRUE)) %>%
+#   add_markers() %>%
+#   layout(scene = list(xaxis = list(title = 'Cost'),
+#                       yaxis = list(title = 'Gamma'),
+#                       zaxis = list(title = 'Accuracy')))
 
 # Train:Test 70:30
-train.df <- data.df[1:262,]
-test.df <- data.df[263:374,]
+train.df <- data.df[1:260,]
+test.df <- data.df[261:374,]
 
 # Best Linear SVM model 2 One of the best AUCs
 classifier <- svm(formula = WL ~ .,
                   data = train.df,
                   type = 'C-classification',
                   kernel = 'linear',
-                  cost = 0.25)
+                  cost = 0.125)
 y_pred = predict(classifier, newdata = test.df[-ncol(test.df)])
 cm = table(test.df[,ncol(test.df)], y_pred)
 print(cm)
 print(mean(test.df[,ncol(test.df)] == y_pred))
+roc.val <- roc(as.numeric(test.df[,ncol(test.df)]), as.numeric(y_pred))
+print(roc.val$auc[1])
 
 
 # Best Radial SVM model 2 (High AUC)
@@ -240,16 +347,20 @@ y_pred = predict(classifier, newdata = test.df[-ncol(test.df)])
 cm = table(test.df[,ncol(test.df)], y_pred)
 print(cm)
 print(mean(test.df[,ncol(test.df)] == y_pred))
-
-
+roc.val <- roc(as.numeric(test.df[,ncol(test.df)]), as.numeric(y_pred))
+print(roc.val$auc[1])
+# 
+# 
 # Best Sigmoid SVM model
 classifier <- svm(formula = WL ~ .,
                   data = train.df,
                   type = 'C-classification',
                   kernel = 'sigmoid',
-                  cost = 16,
-                  gamma = 0.015625)
+                  cost = 64,
+                  gamma = 0.001953125)
 y_pred = predict(classifier, newdata = test.df[-ncol(test.df)])
 cm = table(test.df[,ncol(test.df)], y_pred)
 print(cm)
 print(mean(test.df[,ncol(test.df)] == y_pred))
+roc.val <- roc(as.numeric(test.df[,ncol(test.df)]), as.numeric(y_pred))
+print(roc.val$auc[1])
